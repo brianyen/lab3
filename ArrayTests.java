9264faa5b619d9377faf5a6fd2a7f9b7dd18b482
brianyen <brianyen1000@gmail.com>
2024-02-01 10:36:12 -0800

    fixed arrayexamples reverse methods

--- ArrayTests.java
+++ ArrayTests.java
@@ -10,9 +10,24 @@ public class ArrayTests {
 	}
 
 
+
   @Test
-  public void testReversed() {
+  public void testReversedEmpty() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+  @Test
+  public void testReversedNonZero() {
+    int[] input1 = {4, 5, 9};
+    assertArrayEquals(new int[] {9, 5, 4}, ArrayExamples.reversed(input1));
+  }
+  @Test
+  public void testReverseInPlaceLonger() {
+    int[] input1 = {5, 9, 19, 4, 2};
+    int[] input2 = {1, 2, 3, 4};
+    ArrayExamples.reverseInPlace(input1);
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] {2, 4, 19, 9, 5}, input1);
+    assertArrayEquals(new int[] {4, 3, 2, 1}, input2);
   }
 }
